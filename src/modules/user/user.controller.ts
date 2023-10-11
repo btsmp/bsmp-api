c44f8d398860b093e5e744009eb0d8b59b769955
Bruno Sampiao B2x <brunoctsb@gmail.com>
2023-10-10 21:00:21 -0300

    feat: create new endpoints to user.module

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Post()
+  async create(@Body() user: CreateUserDTO) {
+    return this.userService.create(user);
+  }
+
+  @Get()
+  list() {
+    return this.userService.listAll();
+  }
+}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(user: CreateUserDTO) {
+    const { name, email, password, role } = user;
+    return this.prisma.user.create({ data: { name, email, password, role } });
+  }
+
+  async listAll() {
+    return this.prisma.user.findMany({});
+  }
+}
