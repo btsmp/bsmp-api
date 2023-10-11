e4d0b2b586004b45352a2fb52bc9899a2d99bb7e
Bruno Sampiao B2x <brunoctsb@gmail.com>
2023-10-11 12:56:07 -0300

    fix: logger is now working for all requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserController } from './modules/user/user.controller';
 import { UserService } from './modules/user/user.service';
 import { UserModule } from './modules/user/user.module';
 import { ProjectsController } from './modules/projects/projects.controller';
-import { ProjectsService } from './modules/projects/projects.service';
+import { ProjectService } from './modules/projects/project.service';
 import { ProjectsModule } from './modules/projects/projects.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaService } from './modules/prisma/prisma.service';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { LoggerMiddleware } from './core/middlewares/logger.middleware';
 
 @Module({
   imports: [UserModule, ProjectsModule, AuthModule, PrismaModule],
   controllers: [UserController, ProjectsController],
-  providers: [UserService, ProjectsService, PrismaService],
+  providers: [UserService, ProjectService, PrismaService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/core/middlewares/logger.middleware.ts
+++ src/core/middlewares/logger.middleware.ts
@@ -4,10 +4,11 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const logger = new Logger();
+    const logger = new Logger('LoggerMiddleware');
     const endpoint = req.baseUrl;
+    const method = req.method;
 
-    logger.log(`New Request => ${endpoint}`);
+    logger.log(`New Request => ${endpoint} | Method: ${method}`);
     next();
   }
 }
